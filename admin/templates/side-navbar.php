a83d10fabd6c166e61d0e55cf1b67d578fc34296
Laurent Boulanger <perso@laurentboulanger.com>
2018-04-15 17:59:10 +0200

    Updated avatar in side-navbar file

--- admin/templates/side-navbar.php
+++ admin/templates/side-navbar.php
@@ -1,7 +1,7 @@
 <nav class="side-navbar">
   <!-- Sidebar Header-->
   <div class="sidebar-header d-flex align-items-center">
-     <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+     <div class="avatar"><img src="img/avatar.png" alt="..." class="img-fluid rounded-circle"></div>
     <div class="title">
       <h1 class="h4">Paul Bernard</h1>
       <p>Admin</p>
