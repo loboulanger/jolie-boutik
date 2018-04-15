78e4993208ecdcb495cd0f6ba08ff061ffbb76de
Laurent Boulanger <perso@laurentboulanger.com>
2018-04-15 18:52:03 +0200

    Updated update-shop file

--- admin/update_shop.php
+++ admin/update_shop.php
@@ -18,9 +18,98 @@ require ('templates/header.php');
             <section class="forms"> 
               <div class="container-fluid">
                 <div class="row">
+                  <!-- Add Picture -->
+                  <div class="col-lg-12">
+                      <div class="card">
+                        <div class="card-header d-flex align-items-center">
+                          <h3 class="h4">Slider de la page d'accueil</h3>
+                        </div>
+                        <div class="card-body">
+                          <div class="table-responsive">
+                            <table class="table">
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>Titre</th>
+                                  <th>Sous-titre</th>
+                                  <th>CTA</th>
+                                  <th>Image</th>
+                                  <th></th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <th scope="row">1</th>
+                                  <td>Titre 1</td>
+                                  <td>Sous-titre 1</td>
+                                  <td>CTA 1</td>
+                                  <td>img-1</td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Modifier</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Supprimer</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">2</th>
+                                  <td>Titre 2</td>
+                                  <td>Sous-titre 2</td>
+                                  <td>CTA 2</td>
+                                  <td>img-2</td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Modifier</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Supprimer</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">3</th>
+                                  <td>Titre 3</td>
+                                  <td>Sous-titre 3</td>
+                                  <td>CTA 3</td>
+                                  <td>img-3</td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Modifier</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                  <td>
+                                      <form class="form-inline">
+                                          <div class="form-group">
+                                              <button type="submit" class="btn btn-primary btn-sm">Supprimer</button>
+                                          </div>
+                                        </form>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
-                  <!-- Form Elements -->
-                  <div class="col-lg-6">
+                  <!-- Coordonnées Boutik-->
+                  <div class="col-lg-5">
                     <div class="card">
                       <div class="card-header d-flex align-items-center">
                         <h3 class="h4">Coordonnées</h3>
@@ -54,67 +143,6 @@ require ('templates/header.php');
                     </div>
                   </div>
 
-                  <!-- Add Picture -->
-                  <div class="col-lg-6">
-                      <div class="card">
-                        <div class="card-header d-flex align-items-center">
-                          <h3 class="h4">Images du slider</h3>
-                        </div>
-                        <div class="card-body">
-                          <div class="table-responsive">
-                            <table class="table">
-                              <thead>
-                                <tr>
-                                  <th>#</th>
-                                  <th>Titre</th>
-                                  <th></th>
-                                  <th></th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <th scope="row">1</th>
-                                  <td>Titre 1</td>
-                                  <td><button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">Voir</button></td>
-                                  <td>
-                                      <form class="form-inline">
-                                          <div class="form-group">
-                                              <button type="submit" class="btn btn-primary">Supprimer</button>
-                                          </div>
-                                        </form>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th scope="row">2</th>
-                                  <td>Titre 2</td>
-                                  <td><button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">Voir</button></td>
-                                  <td>
-                                    <form class="form-inline">
-                                      <div class="form-group">
-                                          <button type="submit" class="btn btn-primary">Supprimer</button>
-                                      </div>
-                                    </form>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th scope="row">3</th>
-                                  <td>Titre 3</td>
-                                  <td><button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">Voir</button></td>
-                                  <td>
-                                      <form class="form-inline">
-                                          <div class="form-group">
-                                              <button type="submit" class="btn btn-primary">Supprimer</button>
-                                          </div>
-                                        </form>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  
                 </div>
               </div>
             </section>
