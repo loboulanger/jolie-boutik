ed6d529315680ab670463cd22a46c7e54efa6d7c
Laurent Boulanger <perso@laurentboulanger>
2018-04-18 19:46:26 +0200

    Updated inc/functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -5,3 +5,8 @@ function debug($variable){
 	print_r($variable);
 	echo "</pre>";
 }
+
+function str_random($length){
+    $alphabet = "1234567890azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+    return substr(str_shuffle(str_repeat($alphabet, $length)), 0, $length);
+}
